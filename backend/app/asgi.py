26cdd367dad8bda5c395b57823703037d7a86046
Aness-Aouissat <anessaouissat@gmail.com>
2025-11-17 03:35:38 +0400

    feat(core): add lifespan hook to create tables and mount notes router

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from .api import notes
+from app.db.init_db import create_db_and_tables
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()                    
+    yield  
 
 def create_app() -> FastAPI:
+    
     app = FastAPI(
         title = "CoursePilot",
-        version = "1.0.0"
+        version = "1.0.0",
+        lifespan=lifespan
     )
+    app.include_router(notes.router)
     return app
 
 app = create_app()
