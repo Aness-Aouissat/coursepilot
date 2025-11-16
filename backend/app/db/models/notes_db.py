360a0462b5c7ff9851a9c0a69f7ddeb8958e2d47
Aness-Aouissat <anessaouissat@gmail.com>
2025-11-17 03:36:28 +0400

    refactor(db): migrate Notes model to SQLAlchemy 2.0 mapping API

--- backend/app/db/models/notes_db.py
+++ backend/app/db/models/notes_db.py
@@ -1,16 +1,16 @@
 import uuid
 
-from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, DateTime, ForeignKey, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import func
 
-from db.session import Base 
+from app.db.session import Base 
 
 class Notes(Base):
     __tablename__ = "notes"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    title = Column(String(255))
-    content = Column(Text)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    id : Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title : Mapped[str] = mapped_column(String(255), nullable=True)
+    content : Mapped[str] = mapped_column(Text, nullable=False, server_default="")
+    created_at : Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at : Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
