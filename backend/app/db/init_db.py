ca856b18f975842e588e32c0a092910b51e761c4
Aness-Aouissat <anessaouissat@gmail.com>
2025-11-19 20:17:04 +0400

    fix(db): resolve missing relationship initialization by importing models before metadata creation

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,4 +1,6 @@
 from .session import engine, Base
+from app.db.models.users_db import Users
+from app.db.models.notes_db import Notes
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
