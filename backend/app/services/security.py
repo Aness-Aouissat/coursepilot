cca029d19bb2d6e02aaa5939b5bde864789ed9a9
Aness-Aouissat <anessaouissat@gmail.com>
2025-11-22 06:21:10 +0400

    feature(security): add create_access_token and generate_complete_user_token helpers

--- backend/app/services/security.py
+++ backend/app/services/security.py
@@ -1,9 +1,10 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone, timedelta
 from pwdlib import PasswordHash
 from dotenv import load_dotenv
 
 import jwt
 import os
+import uuid
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -13,25 +14,22 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 password_hash = PasswordHash.recommended()
 
 def verify_password(plain_password, hashed_password):
-    """
-    To be used in user log in. The input password will be verified
-    against the hashed password stored in the database before granting 
-    the user access.
-    """
     return password_hash.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    """
-    To be used in user registration. The input password will be 
-    hashed before being stored in the database to be used for future
-    verifications.
-    """
     return password_hash.hash(password)
 
-def create_access_token():
-    pass
+def create_access_token(data: dict, expires_delta: timedelta):
+    copy_of_data = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    copy_of_data.update({"exp": expire})
+    access_token = jwt.encode(copy_of_data, SECRET_KEY, algorithm=ALGORITHM)
 
-def decode_token():
-    pass
+    return access_token
 
+def generate_complete_user_token(user_id: uuid.UUID):
+    sub = {"sub": str(user_id)}
+    exp = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(sub, exp) 
 
+    return access_token
