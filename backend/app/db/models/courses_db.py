d5ccbf7cd95a61b06f7601c6f55fb2a5f550fefd
Aness-Aouissat <anessaouissat@gmail.com>
2025-11-24 23:26:04 +0400

    feat(db): add Courses model, connect Notes to Courses, fix nullable title field, and update relationships

--- backend/app/db/models/courses_db.py
+++ backend/app/db/models/courses_db.py
@@ -1,25 +1,24 @@
 import uuid
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING 
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Text, DateTime, ForeignKey, func
+from sqlalchemy import String, DateTime, ForeignKey, func
 from sqlalchemy.dialects.postgresql import UUID
 
 from app.db.session import Base
 
 if TYPE_CHECKING:
     from .users_db import Users
-    from .courses_db import Courses
+    from .notes_db import Notes
 
-class Notes(Base):
-    __tablename__ = "notes_table"
-    user: Mapped["Users"] = relationship(back_populates="notes")
+class Courses(Base):
+    __tablename__ = "courses_table"
+    user: Mapped["Users"] = relationship(back_populates="courses")
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users_table.id", ondelete="CASCADE"), nullable=False)
-    course: Mapped["Courses"] = relationship(back_populates="notes")
-    course_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("courses_table.id", ondelete="CASCADE"), nullable=False)
+    notes: Mapped["Notes"] = relationship(back_populates="course")
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title: Mapped[str] = mapped_column(String(255))
-    content: Mapped[str] = mapped_column(Text, nullable=False, server_default="")
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
--- backend/app/db/models/users_db.py
+++ backend/app/db/models/users_db.py
@@ -8,18 +8,20 @@ from sqlalchemy.dialects.postgresql import UUID
 from app.db.session import Base
 
 if TYPE_CHECKING:
+    from .courses_db import Courses
     from .notes_db import Notes
 
 class Users(Base):
     __tablename__ = "users_table"
     notes: Mapped[list["Notes"]] = relationship(back_populates="user", cascade="all, delete")
+    courses: Mapped[list["Courses"]] = relationship(back_populates="user", cascade="all, delete")
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(String[50])
     first_name: Mapped[str] = mapped_column(String[50])
     last_name: Mapped[str] = mapped_column(String[50])
-    email: Mapped[str] = mapped_column(String[100])
-    hashed_password: Mapped[str] = mapped_column(String[175])
+    email: Mapped[str] = mapped_column(String[125])
+    hashed_password: Mapped[str] = mapped_column(String[255])
     age: Mapped[int] = mapped_column(Integer)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     disabled: Mapped[Boolean] = mapped_column(Boolean, default=False)
